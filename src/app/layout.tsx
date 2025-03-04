2c2806025b3891dc48b05f2ff3c7794b45d9c233
SziNo <szino1848@gmail.com>
2025-03-04 17:58:20 +0100

    add favicon + cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
-import HeadComponent from '../components/layout/head'
+import HeadComponent from '@/components/layout/head'
 import './globals.css'
 
 const geistSans = Geist({
